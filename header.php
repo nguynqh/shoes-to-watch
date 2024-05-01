375de10e108efab97d2c6eab12db2006807206dd
Nguyễn Quang Hiếu <mequanghieu9@gmail.com>
2024-05-01 23:13:56 +0700

    update db
    them bang manager

---
+++ header.php
@@ -0,0 +1,74 @@
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4" style="background-color: rgb(54, 54, 54);text-align: center">
+                <div class="login">
+                    <!-- kiem tra dang nhap -->
+                    <?php
+                    session_start();
+                    if (!isset($_SESSION['user'])) {
+                    ?>
+                        <a href="../tai-khoan/dang-nhap.php">
+                            <p><strong>ĐĂNG NHẬP / ĐĂNG KÍ</strong></p>
+                        </a>
+                    <?php } else { ?>
+                        <a href="../tai-khoan/thong-tin-tk.php">
+                            <p><strong>XIN CHÀO <?= $_SESSION['user']['ho_ten'] ?></strong></p>
+                        </a>
+                    <?php } ?>
+                    
+                </div>
+            </div>
+            <div class="col-md-4" style="background-color: rgb(54, 54, 54);text-align: center">
+                <div class="logo">
+                    <a href="../index.php"><img src="../css/trang-chu/img/iconweb.png" alt="anh"></a>
+                </div>
+            </div>
+            <div class="col-md-4" style="background-color: rgb(54, 54, 54);text-align: center">
+                <div class="giohang" style="position: reletive;">
+
+                    <?php
+                    $sll = 0;
+                    if (isset($_SESSION['cart'])) {
+                        foreach ($_SESSION['cart'] as $item) {
+                            extract($item);
+                            $sll += $sl;
+                        }
+                    }
+                    ?>
+                    <span style="position: absolute;padding:3px 8px;background-color:#fff;border-radius:50px;left:295px;top:25px;"><?= $sll ?></span>
+                    <ul>
+                        <li style="list-style: none;">
+                            <p style="font-size: 14px;">
+                                <a href="don-mua.php" style="text-decoration:none;color:#f7941d;">ĐƠN MUA</a>
+                            </p>
+                        </li>
+                        <li style="list-style: none;">
+                            <p style="color: rgb(212, 212, 212);font-size: 14px;">GIỎ HÀNG</p>
+                        </li>
+                        <a href="danh-sach-gio-hang.php">
+                            <li style="list-style: none;">
+                                <i class="fa fa-shopping-basket" style="font-size:28px;color:rgb(255, 255, 255)"></i>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12" style="background-color: rgb(211, 211, 211);">
+                <div class="nav">
+                    <ul>
+                        <li><a href="../index.php">TRANG CHỦ</a></li>
+                        <li><a href="danh-sach-sp.php">SẢN PHẨM</a></li>
+                        <li><a href="gioi-thieu.php">GIỚI THIỆU</a></li>
+                        <li><a href="bao-hanh.php">BẢO HÀNH</a></li>
+                        <li><a href="lien-he.php">LIÊN HỆ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
--- trang-chinh/tim-kiem-theo-ten.php
+++ trang-chinh/tim-kiem-theo-ten.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../bigshoes/css/danh-sach-sp/products.css">
+    <link rel="stylesheet" href="../css/danh-sach-sp/products.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../bigshoes/css/danh-sach-sp/plugin/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="../../bigshoes/css/danh-sach-sp/plugin/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../css/danh-sach-sp/plugin/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/danh-sach-sp/plugin/css/owl.theme.default.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" type="image/png" href="../css/trang-chu/img/TBT.png" />
     <title>Sản phẩm</title>
@@ -19,7 +19,7 @@
 
 <body>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script type="text/javascript" src="../../bigshoes/css/danh-sach-sp/plugin/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../css/danh-sach-sp/plugin/js/owl.carousel.min.js"></script>
 
     <style>
         .sanpham {
@@ -101,88 +101,11 @@
         }
     </style>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-4" style="background-color: rgb(54, 54, 54);text-align: center">
-                <div class="login">
-                    <?php
-                    session_start();
-                    if (!isset($_SESSION['user'])) {
-                    ?>
-                        <a href="../../bigshoes/tai-khoan/dang-nhap.php">
-                            <p><strong>ĐĂNG NHẬP / ĐĂNG KÍ</strong></p>
-                        </a>
-                    <?php } else { ?>
-                        <a href="../../bigshoes/tai-khoan/thong-tin-tk.php">
-                            <p><strong>XIN CHÀO <?= $_SESSION['user']['ho_ten'] ?></strong></p>
-                        </a>
-                    <?php } ?>
-                </div>
-
-            </div>
-            <div class="col-md-4" style="background-color: rgb(54, 54, 54);text-align: center">
-                <div class="logo">
-                    <a href="index.php"><img src="../css/danh-sach-sp/img/TBT.png" alt="anh"></a>
-                </div>
-            </div>
-            <div class="col-md-4" style="background-color: rgb(54, 54, 54);text-align: center">
-                <div class="giohang" style="position: reletive;">
-
-                    <?php
-                    $sll = 0;
-                    if (isset($_SESSION['cart'])) {
-                        foreach ($_SESSION['cart'] as $item) {
-                            extract($item);
-                            $sll += $sl;
-                        }
-                    }
-                    ?>
-                    <span style="position: absolute;padding:3px 8px;background-color:#fff;border-radius:50px;left:295px;top:25px;"><?= $sll ?></span>
-                    <ul>
-                        <li style="list-style: none;">
-                            <p style="font-size: 14px;">
-                                <a href="don-mua.php" style="text-decoration:none;color:#f7941d;">ĐƠN MUA</a>
-                            </p>
-                        </li>
-                        <li style="list-style: none;">
-                            <p style="color: rgb(212, 212, 212);font-size: 14px;">GIỎ HÀNG</p>
-                        </li>
-                        <a href="danh-sach-gio-hang.php">
-                            <li style="list-style: none;">
-                                <i class="fa fa-shopping-basket" style="font-size:28px;color:rgb(255, 255, 255)"></i>
-                            </li>
-                        </a>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- header -->
+    <?php require('../header.php');?>
 
 
-
-
-
-
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12" style="background-color: rgb(211, 211, 211);">
-                <div class="nav">
-                    <ul>
-                        <li><a href="index.php">TRANG CHỦ</a></li>
-                        <li><a href="danh-sach-sp.php">SẢN PHẨM</a></li>
-                        <li><a href="gioi-thieu.php">GIỚI THIỆU</a></li>
-                        <li><a href="bao-hanh.php">BẢO HÀNH</a></li>
-                        <li><a href="lien-he.php">LIÊN HỆ</a></li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-
-    <div class="container" style="margin-top: 20px;">
+    <!-- <div class="container" style="margin-top: 20px;">
         <div class="row">
             <div class="col-md-7" style="padding-left: 0px;">
                 <div class="chuyen">
@@ -207,14 +130,13 @@
 
             </div>
         </div>
-    </div>
+    </div> -->
 
 
 
     <div class="container">
         <div class="row">
             <div class="col-md-3" style="padding-left: 0px;margin-top: 42px;">
-
                 <div class="row" style="margin-left: 0px;">
                     <form action="tim-kiem-theo-ten.php" method="post">
                         <div class="serch">
@@ -225,7 +147,7 @@
                 </div>
                 <div class="row" style="margin-left: 0px;">
                     <?php
-                    require_once('../../bigshoes/admin/dao/loai-hang.php');
+                    require_once('../admin/dao/loai-hang.php');
                     extract($_REQUEST);
                     $items = loai_hang_select_all();
                     ?>
@@ -248,7 +170,7 @@
 
                         <ul class="list-group">
                             <?php
-                            require_once('../../bigshoes/admin/dao/hang-hoa.php');
+                            require_once('../admin/dao/hang-hoa.php');
                             extract($_REQUEST);
                             $items = hang_hoa_sale();
 
@@ -281,7 +203,7 @@
 
                 ?>
                     <div class="sanpham">
-                        <a href="chi-tiet-sp.php?ma_hh=<?= $ma_hh ?>"><img src="../../bigshoes/css/admin/images/products/<?= $hinh ?>" alt=""></a>
+                        <a href="chi-tiet-sp.php?ma_hh=<?= $ma_hh ?>"><img src="../hinh-anh/san-pham/<?= $hinh ?>" alt=""></a>
                         <div class="text">
                             <div class="name">
                                 <?= $ten_hh ?>
